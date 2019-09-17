1ec48d57fabf52a5ad9c9f56bf13444182d6a4de
thanhduong.doan <thanhduong.doan@nhnent.com>
2019-09-17 16:11:10 +0700

    change Failure to data class

--- common_jvm/src/main/java/com/example/common/exception/Failure.kt
+++ common_jvm/src/main/java/com/example/common/exception/Failure.kt
@@ -3,9 +3,9 @@ package com.example.common.exception
 sealed class Failure {
     object InternetError : Failure()
     object ConnectionTimeout : Failure()
-    class ApiError(val httpCode: Int, val errorMsg: String) : Failure()
-    class ServerError(val errorCode: Int, val errorMsg: String) : Failure()
-    class UnCatchError(val exception: Exception) : Failure()
+    data class ApiError(val httpCode: Int, val errorMsg: String) : Failure()
+    data class ServerError(val errorCode: Int, val errorMsg: String) : Failure()
+    data class UnCatchError(val exception: Exception) : Failure()
 
     /** * Extend this class for feature specific failures.*/
     abstract class FeatureFailure : Failure()
