18bc790a8726040dc683495ffc3c60f6a77710ad
Cuognnv <vancuong0429@gmail.com>
2019-09-17 16:29:08 +0700

    change resource to either in detail view

--- data/repository/src/main/java/com/example/repository/DetailRepositoryImpl.kt
+++ data/repository/src/main/java/com/example/repository/DetailRepositoryImpl.kt
@@ -1,27 +1,20 @@
 package com.example.repository
 
-import com.example.domain.entities.Resource
+import com.example.common.exception.Failure
+import com.example.common.functional.Either
 import com.example.domain.entities.UserEntity
 import com.example.domain.repositories.UserDetailRepository
 import com.example.remote.UserDataSource
+import com.example.remote.exception_interceptor.RemoteExceptionInterceptor
 import com.example.repository.mapper.UserEntityMapper
 
-class DetailRepositoryImpl(private val userDataSource: UserDataSource) : UserDetailRepository {
+class DetailRepositoryImpl(private val userDataSource: UserDataSource, private val remoteExceptionInterceptor: RemoteExceptionInterceptor) : UserDetailRepository {
     private val userEntityMapper = UserEntityMapper()
-    override suspend fun getUserDetail(login: String): Resource<UserEntity> {
+    override suspend fun getUserDetail(login: String): Either<Failure, UserEntity> = Either.runSuspendWithCatchError(errorInterceptors = listOf(remoteExceptionInterceptor)){
         var response = userDataSource.fetchUserDetailsAsync(login).run {
             userEntityMapper.mapResponseToEntity(this)
         }
-        return try {
-
-            if (response == null) {
-                Resource(Resource.Status.ERROR, null, null)
-            } else {
-                Resource(Resource.Status.SUCCESS, response, null)
-            }
-        } catch (e: Exception) {
-            Resource(Resource.Status.ERROR, null, e)
-        }
+        return@runSuspendWithCatchError Either.Success(response)
     }
 
 }
--- domain/src/main/java/com/example/domain/usecases/GetUserDetailUseCase.kt
+++ domain/src/main/java/com/example/domain/usecases/GetUserDetailUseCase.kt
@@ -1,11 +1,12 @@
 package com.example.domain.usecases
 
-import com.example.domain.entities.Resource
+import com.example.common.exception.Failure
+import com.example.common.functional.Either
 import com.example.domain.entities.UserEntity
 import com.example.domain.repositories.UserDetailRepository
 
 class GetUserDetailUseCase(private val detailRepository: UserDetailRepository){
-    suspend fun run(login: String): Resource<UserEntity> {
+    suspend fun run(login: String): Either<Failure, UserEntity> {
         return detailRepository.getUserDetail(login)
     }
 }
