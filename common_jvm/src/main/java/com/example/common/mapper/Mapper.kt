b921a08da705abd411988e4fe8dee370e8de6188
thanhduong.doan <thanhduong.doan@nhnent.com>
2019-09-18 09:21:43 +0700

    add mapper class

---
+++ common_jvm/src/main/java/com/example/common/mapper/Mapper.kt
@@ -0,0 +1,13 @@
+package com.example.common.mapper
+
+abstract class Mapper<Input, Output> {
+    abstract fun map(input: Input) : Output
+
+    fun mapList(inputs: List<Input>?) : List<Output> {
+        val outputs = mutableListOf<Output>()
+        inputs?.forEach { input: Input ->
+            outputs.add(map(input))
+        }
+        return outputs
+    }
+}
